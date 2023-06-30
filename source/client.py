5e5eb77ad855ade2fce1082fece913a9959b0223
Lev <vinnipyx38@gmail.com>
2023-06-30 15:18:27 +0500

    Added files

---
+++ source/client.py
@@ -0,0 +1,66 @@
+import os
+import logging
+import time
+import traceback
+
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+file_log = logging.FileHandler('log_client.log', encoding='utf-8')
+logging.basicConfig(handlers=(file_log,),
+                    format='[%(asctime)s | %(levelname)s | %(name)s]: %(message)s',
+                    datefmt='%m.%d.%Y %H:%M:%S',
+                    level=logging.INFO)
+
+log = logging.getLogger("parser")
+
+
+class Client:
+    categories = (("https://www.ozon.ru/category/protsessory-15726/", "Процессоры"),
+                  ('https://www.ozon.ru/category/smartfony-15502/', 'Смартфоны'))
+
+    def __init__(self):
+        self.driver = uc.Chrome(service=Service(ChromeDriverManager().install()), headless=True,
+                                user_data_dir=f"{os.getcwd()}/cookie")
+        self.driver.implicitly_wait(5)
+        self.driver.set_window_size(1920, 1080)
+
+    def get_urls(self):
+        try:
+            result = []
+            for category, name in self.categories:
+                urls = []
+                for i in range(5):
+                    url = category
+                    if i != 0:
+                        url += f'?page={i + 1}'
+                    self.driver.get(url)
+                    time.sleep(1)
+                    product_url = list(map(lambda x: x.get_attribute("href"),
+                                           self.driver.find_elements(By.CLASS_NAME, 'tile-hover-target.i3l.il4')))
+                    while len(product_url) < 20 and i != 4:
+                        time.sleep(1)
+                        product_url = list(map(lambda x: x.get_attribute("href"),
+                                               self.driver.find_elements(By.CLASS_NAME, 'tile-hover-target.i3l.il4')))
+
+                    product_price = list(map(lambda x: x.text.split("₽")[0].replace('\u2009', ''),
+                                             self.driver.find_elements(By.CLASS_NAME, 'oi6')))
+                    urls.extend((product_url, product_price))
+                result.append((urls, name))
+            return result
+        except Exception as ex:
+            print('Произошла ошибка внутри селениума, необходимо посмотреть файл log_client.log')
+            log.error({'error': ex, 'traceback': traceback.format_exc()})
+
+    def get_data(self, url):
+        try:
+            self.driver.get(url)
+            return self.driver.find_element(By.TAG_NAME, 'body').text
+        except Exception as ex:
+            print('Произошла ошибка внутри селениума, необходимо посмотреть файл log_client.log')
+            log.error({'error': ex, 'traceback': traceback.format_exc()})
+
+    def quit(self):
+        self.driver.quit()
---
+++ source/main.py
@@ -0,0 +1,82 @@
+import json
+import logging
+import traceback
+
+from client import Client
+
+
+file_log = logging.FileHandler('log_client.log', encoding='utf-8')
+logging.basicConfig(handlers=(file_log,),
+                    format='[%(asctime)s | %(levelname)s | %(name)s]: %(message)s',
+                    datefmt='%m.%d.%Y %H:%M:%S',
+                    level=logging.INFO)
+
+log = logging.getLogger("parser")
+
+api_url = "https://www.ozon.ru/api/entrypoint-api.bx/page/json/v2?url={}/?layout_container=pdpPage2column&layout_page_index=2"
+
+
+def parse_elem(data, photo, price):
+    characteristics = {'price': price, 'photo': photo}
+    name = data['productTitle'].replace('Характеристики:', '').strip()
+    data = data['characteristics']
+    for characteristic in data:
+        for elem in characteristic['short']:
+            if 'OZON' not in elem['name']:
+                characteristics[elem['name']] = elem['values'][0]['text']
+    return name, characteristics
+
+
+def parse_json(data, price):
+    data = json.loads(data)
+    photo = list(filter(lambda x: '.jpg' in x['content'], data['seo']['meta']))[0]['content']
+    data = data['widgetStates']
+    for widget_name, widget_value in data.items():
+        if 'webCharacteristics' in widget_name:
+            elem = json.loads(widget_value)
+            return parse_elem(elem, photo, price)
+
+
+def parse_urls(urls, client):
+    result = {}
+    for data, name in urls:
+        result_category = {}
+        print(f"Парсинг категории '{name}'")
+        category_urls, category_price = data
+        for i, url in enumerate(category_urls):
+            print(f'Парсинг товара {i + 1}/{len(category_urls)}')
+            url = url.split("/?")[0].replace("https://www.ozon.ru", '')
+            data = client.get_data(api_url.format(url))
+            product_name, characteristics = parse_json(data, category_price[i])
+            result_category[product_name] = characteristics
+
+        result[name] = result_category
+    return result
+
+
+def save_result(result):
+    with open('data.json', 'w', encoding='utf-8') as fp:
+        json.dump(result, fp, ensure_ascii=False)
+
+
+def main():
+    try:
+        print('Начало работы парсера')
+        client = Client()
+
+        urls = client.get_urls()
+
+        result = parse_urls(urls, client)
+
+        client.quit()
+
+        save_result(result)
+        print('Парсер успешно завершил работу')
+    except Exception as ex:
+        print('Произошла ошибка, необходимо посмотреть файл log.log')
+        log.error({'error': ex, 'traceback': traceback.format_exc()})
+
+
+if __name__ == '__main__':
+    main()
+    input('')
